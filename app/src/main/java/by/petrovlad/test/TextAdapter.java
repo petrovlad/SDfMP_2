08435df0fb29f1e0544d486bd34b539b8c0944b0
petrovlad <vladislav_petrovec@mail.ru>
2021-04-22 18:51:56 +0300

    change adapter input (now it is a whole kitten)

--- app/src/main/java/by/petrovlad/test/TextAdapter.java
+++ app/src/main/java/by/petrovlad/test/TextAdapter.java
@@ -18,20 +18,22 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 public class TextAdapter extends BaseAdapter {
+
     private Context context;
-    private List<String> strings;
+    private List<Kitten> kittens;
     private LayoutInflater inflater;
     private Typeface typeface;
 
-    public TextAdapter(Context c, List<String> strings) {
+
+    public TextAdapter(Context c, List<Kitten> kittens) {
         this.context = c;
-        this.strings = strings;
+        this.kittens = kittens;
         inflater = LayoutInflater.from(c);
     }
 
     @Override
     public int getCount() {
-        return strings.size();
+        return kittens.size();
     }
 
     @Override
@@ -48,16 +50,24 @@ public class TextAdapter extends BaseAdapter {
     public View getView(int position, View view, ViewGroup parent) {
         typeface = ResourcesCompat.getFont(context, FontSettings.fontId);
 
-        view = inflater.inflate(R.layout.text_grid_item, null);
+        view = inflater.inflate(R.layout.text_grid_item, parent, false);
 
         ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
         layoutParams.setMargins(FontSettings.fontSize, FontSettings.fontSize, FontSettings.fontSize, FontSettings.fontSize);
+
+        Kitten kitten = kittens.get(position);
+
+        String text = context.getString(R.string.kitten_name) + ": " + kitten.name + "\n"
+                + context.getString(R.string.kitten_eyes_color) + ": " + kitten.eyesColor + "\n"
+                + context.getString(R.string.kitten_tail_length) + ": " + kitten.tailLength + "\n"
+                + context.getString(R.string.kitten_height) + ": "  + kitten.height;
 
         TextView textView = view.findViewById(R.id.tvGridItem);
         textView.setTextSize(FontSettings.fontSize);
         textView.setTypeface(typeface);
         textView.setLayoutParams(layoutParams);
-        textView.setText(strings.get(position));
+        textView.setText(text);
+
         return view;
     }
 }
